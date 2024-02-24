45cb9863e5263864a1a2d373014999369c213c55
Mohana Prakash <mohan@ggexcel.ai>
2024-02-24 15:19:26 +0530

    text chage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //Output directory
+  output: "export",
+  //Trailing slash
+  trailingSlash: true,
+  //React strict mode
+  reactStrictMode: true,
+  //Target Directory for build
+  distDir: "build",
+  //Image Optimization (As of now, it is disabled)
+  images: {
+    unoptimized: true,
+  },
+  //Environment variables
+  env: {
+    AUTH_BASE_URL: "https://api.envision.colan.in/envision-lxpj/auth",
+    ADMIN_BASE_URL: "https://api.envision.colan.in/envision-lxpj/admin",
+    USER_BASE_URL: "https://api.envision.colan.in/envision-lxpj/user",
+    PROD_BASE_URL: "https://api.envision.colan.in/envision-lxpj",
+  },
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            dfgdfgdgd{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
